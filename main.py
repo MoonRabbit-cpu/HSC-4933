9b665dc9b99654e5be0a17aca8ac1db468cf135b
MoonRabbit-cpu <Adambees@proton.me>
2026-08-26 10:44:38 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+########################################
+# Bryan Hernandez Rodriguez -----------#
+# main.py: Print hello world           #
+# examines behavior of data types      #
+########################################
+print("Hello, World!")
+a = "hello world"
+print(a)
